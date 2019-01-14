0d4ec46b71dc8edccef6012b62d156d8236628e1
Ashar <ekicenx@gmail.com>
2019-01-14 23:16:04 +0700

    List Cluster

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_from_api
+  
   def edit_user
     @pages = { page: "edit_user" }
     render "pages/users/edit_user"
@@ -13,4 +15,17 @@ class UsersController < ApplicationController
     @pages = { page: "list_user" }
     render "pages/users/list_user"
   end
+
+  def list_cluster
+    page = params[:page]
+    @list_cluster = @cluster.get_cluster_lists(page, 30)["data"]["clusters"]
+
+    @pages = { page: "list_user_cluster" }
+    render "pages/users/list_user_cluster"
+  end
+
+  private
+    def set_from_api
+      @cluster = Api::Auth::Cluster.new
+    end
 end
--- app/models/api/auth/cluster.rb
+++ app/models/api/auth/cluster.rb
@@ -1,7 +1,7 @@
 class Api::Auth::Cluster < InitApiAuth
 
   def get_cluster_lists(page, per_page)
-    @options = { query: {page: page, per_page: per_page}, headers: {Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}}
+    @options = { query: {page: page, per_page: per_page}, headers: {Authorization: "Bearer 82d65c859b05978f4b3674495f7542e46a679507f5daa36c2ed485440781f7e1"}}
     self.class.get("/dashboard/v1/clusters", @options)
   end
 
