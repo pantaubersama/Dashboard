ddcc5be3316e2f540ec3416468723793d686caff
Ashar <ekicenx@gmail.com>
2019-01-16 15:43:16 +0700

    filter janji politik

--- app/controllers/timeline/politics_controller.rb
+++ app/controllers/timeline/politics_controller.rb
@@ -2,8 +2,13 @@ class Timeline::PoliticsController < ApplicationController
   before_action :set_api
 
   def index
-    @janji_politiks = @janji.get_politics["data"]["janji_politiks"]
-    @total_records = @janji.get_politics["data"]["janji_politiks"].count
+    if params[:filter].present? || params[:q].present? || params[:cluster_id].present?
+      @janji_politiks = @janji.filter_politics(params[:filter], params[:q], params[:cluster_id])["data"]["janji_politiks"]
+      @total_records = @janji.filter_politics(params[:filter], params[:q], params[:cluster_id])["data"]["janji_politiks"].count
+    else
+      @janji_politiks = @janji.get_politics["data"]["janji_politiks"]
+      @total_records = @janji.get_politics["data"]["janji_politiks"].count
+    end
 
     @pages = { page: "index" }
     render "pages/timeline/politics/index"
--- app/models/api/pemilu/janji_politik.rb
+++ app/models/api/pemilu/janji_politik.rb
@@ -1,9 +1,14 @@
 class Api::Pemilu::JanjiPolitik < InitApiPemilu
   include ActiveModel::Model
-  attr_accessor :id
+  attr_accessor :id, :filter, :q, :cluster_id
 
   def get_politics
     @options = { headers: {Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}}
+    self.class.get("/linimasa/v1/janji_politiks", @options)
+  end
+
+  def filter_politics(filter, q, cluster_id)
+    @options = { query: {filter_by: filter, q: q, cluster_id: cluster_id}, headers: {Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}}
     self.class.get("/linimasa/v1/janji_politiks", @options)
   end
 
