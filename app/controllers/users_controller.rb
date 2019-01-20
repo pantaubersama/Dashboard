55fa1a2978263a17caf93a186c397f7dfcfa9d6c
burhanudin hakim <udnpico@gmail.com>
2019-01-21 03:57:08 +0700

    Update detail user cluster

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -96,6 +96,11 @@ class UsersController < ApplicationController
   end
 
 
+  def detail_user_cluster
+    render "pages/users/detail_user_cluster"
+  end
+
+
   private
     def set_api_user
       @user_api = Api::Auth::User.new
