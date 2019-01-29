0e7d53ab97515931cf18508f2f29b98bba102850
Yunan Helmy <helmy@extrainteger.com>
2019-01-29 22:39:15 +0700

    authorization manual per moethod

--- app/models/api/pemilu/quiz.rb
+++ app/models/api/pemilu/quiz.rb
@@ -1,11 +1,7 @@
 class Api::Pemilu::Quiz < InitApiPemilu
-  AUTHORIZATION = {
-    Authorization: "Bearer #{RequestStore.store[:my_api_token]}"
-  }
-
   def all(page=1, per_page=25, q=nil, o="and", m="word_start")
     options = {
-      headers: {}.merge(AUTHORIZATION),
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}),
       query: { q: q }.merge(pagination(page, per_page))
     }
     self.class.get("/dashboard/v1/quizzes", options)
@@ -13,21 +9,21 @@ class Api::Pemilu::Quiz < InitApiPemilu
 
   def find id
     options = {
-      headers: {}.merge(AUTHORIZATION)
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"})
     }
     self.class.get("/dashboard/v1/quizzes/#{id}", options)
   end
 
   def get_question id
     options = {
-      headers: {}.merge(AUTHORIZATION)
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"})
     }
     self.class.get("/dashboard/v1/quizzes/#{id}/questions", options)
   end
 
   def trash(page=1, per_page=25)
     options = {
-      headers: {}.merge(AUTHORIZATION),
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}),
       query: {}.merge(pagination(page, per_page))
     }
     self.class.get("/dashboard/v1/quizzes/trash", options)
@@ -35,28 +31,28 @@ class Api::Pemilu::Quiz < InitApiPemilu
 
   def publish id
     options = {
-      headers: {}.merge(AUTHORIZATION),
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}),
     }
     self.class.post("/dashboard/v1/quizzes/#{id}/publish", options)
   end
   
   def draft id
     options = {
-      headers: {}.merge(AUTHORIZATION),
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}),
     }
     self.class.post("/dashboard/v1/quizzes/#{id}/draft", options)
   end
 
   def destroy id
     options = {
-      headers: {}.merge(AUTHORIZATION),
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}),
     }
     self.class.delete("/dashboard/v1/quizzes/#{id}", options)
   end
 
   def create quiz
     options = {
-      headers: {}.merge(AUTHORIZATION),
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}),
       body: quiz.to_hash
     }
     self.class.post("/dashboard/v1/quizzes/full", options)
