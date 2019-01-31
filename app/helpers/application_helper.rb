74ed847ebe5ac5cbdc5b3daedd6b3ae03af6e73e
myusuf <muhammadyusuf931@gmail.com>
2019-01-31 16:00:27 +0700

    add helper current class active menu sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,13 @@ module ApplicationHelper
   def publish_or_draft p
     p == "published" ? "draft" : "published"
   end
-  
+
   def selected x, y
     "selected" if x.to_s == y.to_s
   end
+
+  def current_class?(test_path)
+      request.path == test_path ? 'active' : ''
+  end
+
 end
