98fd9094a1220300bd82e835e91f52afcaef3ddc
Ashar Mubasir <asharmubasir@gmail.com>
2019-01-31 18:31:50 +0700

    Filter user by cluster

--- app/controllers/user_clusters_controller.rb
+++ app/controllers/user_clusters_controller.rb
@@ -5,7 +5,8 @@ class UserClustersController < ApplicationController
   def index
     @init_user_cluster = @user_cluster.all(params[:page_user].present? ? params[:page_user] : 1,
                           Pagy::VARS[:items], params[:nama].present? ? params[:nama] : "*",
-                          "and", "word_start", params[:verified].present? ? params[:verified] : '')
+                          "and", "word_start", params[:verified].present? ? params[:verified] : '',
+                          params[:cluster_id].present? ? params[:cluster_id] : "" )
 
     n = @init_user_cluster['data']['meta']['pages']['total']
     @total_records = (n*Pagy::VARS[:items])
@@ -17,7 +18,7 @@ class UserClustersController < ApplicationController
 
     @users_clusters = @init_user_cluster["data"]["users"]
 
-    last_page = @user_cluster.all(n, Pagy::VARS[:items], nil, nil, nil, nil)['data']['users'].size
+    last_page = @user_cluster.all(n, Pagy::VARS[:items], nil, nil, nil, nil, nil)['data']['users'].size
     @total_users_clusters = (@total_records - Pagy::VARS[:items]) + last_page
 
     init_verification = [
--- app/models/api/auth/users_clusters.rb
+++ app/models/api/auth/users_clusters.rb
@@ -1,8 +1,8 @@
 class Api::Auth::UsersClusters < InitApiAuth
-	def all(page=1, per_page=30, q="*", o="and", m="word_start", filter_by="")
+	def all(page=1, per_page=30, q="*", o="and", m="word_start", filter_by="", cluster_id)
     options = { 
       query: { 
-        page: page, per_page: per_page, q: q, o: o, m: m, filter_by: filter_by 
+        page: page, per_page: per_page, q: q, o: o, m: m, filter_by: filter_by, cluster_id: cluster_id 
       },
       headers: {
         Authorization: "Bearer #{RequestStore.store[:my_api_token]}"
