3b62b5d26890bd2b89e5f1e6acee78b7c572f306
Yunan Helmy <helmy@extrainteger.com>
2019-02-05 18:16:40 +0700

    merapikan sidebar menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
   def selected x, y
     "selected" if x.to_s == y.to_s
   end
+
+  def active_menu active_link
+    controller_name == active_link ? 'active' : ''
+  end
+  
 end
