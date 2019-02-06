c73e9e07ecf0b37a27f361157d48bc749dc52365
myusuf <muhammadyusuf931@gmail.com>
2019-02-06 15:47:33 +0700

    list & show broadcast

--- app/models/api/notification/broadcast.rb
+++ app/models/api/notification/broadcast.rb
@@ -1,4 +1,4 @@
-class Api::Notification::Notification < InitApiNotification
+class Api::Notification::Broadcast < InitApiNotification
   def create title, description, link, event_type
     options = {
       headers: {Authorization: "Bearer #{RequestStore.store[:my_api_token]}"},
@@ -12,11 +12,24 @@ class Api::Notification::Notification < InitApiNotification
     self.class.post("/dashboard/v1/broadcasts", options)
   end
 
-  def all
+  def all title, page, per_page
     options = {
       headers: {Authorization: "Bearer #{RequestStore.store[:my_api_token]}"},
+      query: {
+        title: title,
+        page: page,
+        per_page: per_page
+      }
     }
-    self.class.get("/dashboard/v1/notifications", options)
+    self.class.get("/dashboard/v1/broadcasts", options)
   end
+
+  def find id
+    options = {
+      headers: {Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}
+    }
+    self.class.get("/dashboard/v1/broadcasts/#{id}", options)
+  end
+
 
 end
