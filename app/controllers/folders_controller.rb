115afd836d8cf154f28cfaae843b9632aeda1f53
Yunan Helmy <helmy@extrainteger.com>
2019-02-07 15:09:23 +0700

    crud folder

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -37,9 +37,57 @@ class FoldersController < ApplicationController
     end
   end
 
+  def create
+    response = @folders_api.create folder_params
+    if response.code == 201
+      flash[:notice] = "Your folder have been created"
+      render json: {notice: "Your folder have been created", status: 201, redirect_to: folders_path}, status: 201
+    else
+      render json: {error: build_error_messages(response), status: 422}, status: 422
+    end
+  end
+
+  def edit
+    response = @folders_api.show params[:id]
+    if response.code == 200
+      render json: {folder: response['data']['question_folder']}
+    else
+      render json: {error: build_error_messages(response), status: 422}, status: 422
+    end
+  end
+
+  def update
+    response = @folders_api.update params[:id], update_folder_params
+    if response.code == 200
+      flash[:notice] = "Your folder have been updated"
+      render json: {notice: "Your folder have been updated", status: 201, redirect_to: folders_path}, status: 201
+    else
+      render json: {error: build_error_messages(response), status: 422}, status: 422
+    end
+  end
+  
+  def show
+    response = @folders_api.show params[:id]
+    if response.code == 200
+      @folder = response['data']['question_folder']
+    else
+      redirect_to folders_path, warning: "Something went wrong"
+    end
+  end
+
   private
     def set_api
       @folders_api = Api::Pemilu::QuestionFolder.new
     end
+
+    def folder_params
+      params.require(:folder).permit!
+    end
+
+    def update_folder_params
+      params.require(:folder).permit!
+    end
+    
+    
 
 end
--- app/models/api/pemilu/question_folder.rb
+++ app/models/api/pemilu/question_folder.rb
@@ -14,7 +14,7 @@ class Api::Pemilu::QuestionFolder < InitApiPemilu
     self.class.get("/dashboard/v1/question_folders", options)
   end
 
-  def destroy(id)
+  def destroy id
     options = {
       headers: {
         Authorization: "#{RequestStore.store[:my_api_token]}"
@@ -22,5 +22,30 @@ class Api::Pemilu::QuestionFolder < InitApiPemilu
     }
     self.class.delete("/dashboard/v1/question_folders/" + id, options)
   end
+
+  def create folder
+    options = {
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}),
+      body: folder.to_hash
+    }
+    self.class.post("/dashboard/v1/question_folders", options)
+  end
+
+  def update id, folder
+    options = {
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"}),
+      body: folder.to_hash
+    }
+    self.class.put("/dashboard/v1/question_folders/" + id, options)
+  end
+
+  def show id
+    options = {
+      headers: {}.merge({Authorization: "Bearer #{RequestStore.store[:my_api_token]}"})
+    }
+    self.class.get("/dashboard/v1/question_folders/" + id, options)
+  end
+  
+  
 
 end
