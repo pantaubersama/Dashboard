d9abd94cc7c9bfe5e848dab274b903aba4ef949d
asharmubasir <ashar@extrainteger.com>
2019-02-21 14:22:01 +0700

    RSpec update tanya kandidat

--- spec/request/menggali/tanya_kandidat_spec.rb
+++ spec/request/menggali/tanya_kandidat_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe "Tanya Kandidat", type: :request do
   let(:id) {SecureRandom.hex}
+  let(:body) {"body"}
+  let(:question_folder_id) {SecureRandom.hex}
+  let(:status) {"status"}
+
   before do
     stub_index_tanya 1, "word_start", "and"
     stub_index_tanya 4, "word_start", "and"
@@ -22,6 +26,20 @@ RSpec.describe "Tanya Kandidat", type: :request do
         get "/questions/#{id}"
         expect(response).to have_http_status(200)
         expect(response).to render_template("questions/show")
+      end
+    end
+
+    describe "Update" do
+      it "can update tanya kandidat record" do
+        stub_update_tanya_kandidat id, body, question_folder_id, status
+        put "/questions/#{id}", params: {
+          id: id,
+          body: body,
+          question_folder_id: question_folder_id,
+          status: status
+        }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(questions_path)
       end
     end
 
--- spec/support/menggali/tanya_kandidat_stubber.rb
+++ spec/support/menggali/tanya_kandidat_stubber.rb
@@ -22,4 +22,16 @@ module TanyaKandidatStubber
       to_return(status: 200, body: data.to_json, headers: {})
   end
 
+  def stub_update_tanya_kandidat id, body, question_folder_id, status
+    file = File.open "#{ENV["PEMILU_STUBBER_PATH"]}/Dashboard/v1/question_actions/id/put.json"
+    data = JSON.parse(file.read)
+    stub_request(:put, "https://staging-pemilu.pantaubersama.com/dashboard/v1/question_actions/#{id}?body=#{body}&id=#{id}&question_folder_id=#{question_folder_id}&status=#{status}").
+      with(
+        # body: "body=#{body}&id=#{id}&question_folder_id=#{question_folder_id}&status=#{status}",
+        headers: {
+      'Authorization'=>''
+        }).
+      to_return(status: 200, body: data.to_json, headers: {})
+  end
+
 end
