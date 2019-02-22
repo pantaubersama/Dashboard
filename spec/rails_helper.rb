986fc55223ab93bedf0e44b40bfc31232bd8a44f
myusuf <muhammadyusuf931@gmail.com>
2019-02-22 11:46:07 +0700

    political party spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,6 +79,7 @@ RSpec.configure do |config|
   config.include TanyaKandidatStubber, type: :request
   config.include BadgesStubber, type: :request
   config.include BannerStubber, type: :request
+  config.include PoliticalPartyStubber, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
